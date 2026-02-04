08f34ddbd51e6593ec28bf502cf2a65bd993e9fc
sulhyun <sulhyun4788@naver.com>
2026-02-04 21:42:58 +0900

    프로젝트 생성

---
+++ quiz-log-server/src/test/java/kr/spring/boot/QuizLogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizLogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
