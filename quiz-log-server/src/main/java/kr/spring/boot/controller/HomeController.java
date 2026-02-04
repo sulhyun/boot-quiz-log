ba151006150b58343ab7a4f5339c3b41f25678d6
sulhyun <sulhyun4788@naver.com>
2026-02-04 22:59:57 +0900

    타임리프 적용

--- quiz-log-server/src/main/java/kr/spring/boot/controller/HomeController.java
+++ quiz-log-server/src/main/java/kr/spring/boot/controller/HomeController.java
@@ -1,14 +1,17 @@
 package kr.spring.boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String main() {
-		return "index";
+	public String home(Model model) {
+		System.out.println("메인 페이지 진입");
+		model.addAttribute("name", "홍길동11");
+		return "home";
 	}
 	
 }
