b802c87374701d179ca6754288211e85b69ab509
sulhyun <sulhyun4788@naver.com>
2026-02-08 18:43:37 +0900

    회원가입 화면 완성

---
+++ quiz-log-server/src/main/java/kr/spring/boot/config/MailConfig.java
@@ -0,0 +1,39 @@
+package kr.spring.boot.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+	@Value("${google.username}")
+	String username;
+	
+	@Value("${google.password}")
+	String password;
+	
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587); // 구글은 587 사용
+
+        mailSender.setUsername(username); 
+        mailSender.setPassword(password); // 앱비밀번호는 직접 입력해서 쓰기  
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true"); 	//보안설정
+        props.put("mail.smtp.starttls.required", "true"); 	// 보안설정
+        props.put("mail.debug", "false");  					// 디버깅을 위해 true 설정
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2"); 	// 보안설정
+
+        return mailSender;
+    }
+}
--- quiz-log-server/src/main/java/kr/spring/boot/controller/MemberController.java
+++ quiz-log-server/src/main/java/kr/spring/boot/controller/MemberController.java
@@ -1,10 +1,14 @@
 package kr.spring.boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.spring.boot.model.vo.MemberVO;
 import kr.spring.boot.service.MemberService;
 
 @Controller
@@ -16,6 +20,15 @@ public class MemberController {
 	
 	@GetMapping("/signup")
 	public String signup() {
+		System.out.println("화원가입 페이지 진입");
 		return "member/signup";
-	}
+	} // 회원 가입 페이지
+	
+	@ResponseBody
+	@GetMapping("/checkId")
+	public ResponseEntity<Boolean> checkId(@RequestParam String mb_id) {
+		MemberVO user = memberService.getMember(mb_id);
+		boolean res = (user == null) ? true : false;
+		return ResponseEntity.ok(res);
+	} // 아이디 중복검사
 }
