c92b3ee98be758204367929b7c02ea1ff682c02f
sulhyun <sulhyun4788@naver.com>
2026-02-10 21:49:35 +0900

    cors 설정 및 아이디 중복검사 수정

--- quiz-log-server/src/main/java/kr/spring/boot/config/SecurityConfig.java
+++ quiz-log-server/src/main/java/kr/spring/boot/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package kr.spring.boot.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import kr.spring.boot.handler.LoginFailHandler;
 import kr.spring.boot.model.util.UserRole;
@@ -17,25 +20,34 @@ public class SecurityConfig{
 
 	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf ->csrf.disable())
-            .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/post/insert/*").hasAuthority(UserRole.USER.name())
-                .requestMatchers("/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
-                .anyRequest().permitAll()  // 그 외 요청은 인증 필요
-            )
-            .formLogin((form) -> form
-                .loginPage("/member/signin")  			// 커스텀 로그인 페이지 설정
-                .permitAll()          		 			// 로그인 페이지는 접근 허용
-                .loginProcessingUrl("/member/signin")	// 로그인 요청을 처리할 URL을 설정
-                .failureHandler(new LoginFailHandler())	
-                .defaultSuccessUrl("/")
-            )
-            .logout((logout) -> logout
-        		.logoutUrl("/member/logout")
-        		.logoutSuccessUrl("/")
-        		.clearAuthentication(true)
-        		.invalidateHttpSession(true)
-        		.permitAll());  // 로그아웃도 모두 접근 가능
+		http
+	        .cors(cors -> cors.configurationSource(request -> {
+	            CorsConfiguration config = new CorsConfiguration();
+	            config.setAllowedOrigins(List.of("*")); 										// 모든 도메인 허용 (테스트용)
+	            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+	            config.setAllowedHeaders(List.of("*"));
+	            config.setAllowCredentials(true); 												// 쿠키나 인증 헤더 허용 시 true
+	            return config;
+	        }))
+	        .csrf(csrf ->csrf.disable())
+	            .authorizeHttpRequests((requests) -> requests
+	                .requestMatchers("/post/insert/*").hasAuthority(UserRole.USER.name())
+	                .requestMatchers("/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
+	                .anyRequest().permitAll()  				// 그 외 요청은 인증 필요
+	            )
+	            .formLogin((form) -> form
+	                .loginPage("/member/signin")  			// 커스텀 로그인 페이지 설정
+	                .permitAll()          		 			// 로그인 페이지는 접근 허용
+	                .loginProcessingUrl("/member/signin")	// 로그인 요청을 처리할 URL을 설정
+	                .failureHandler(new LoginFailHandler())	
+	                .defaultSuccessUrl("/")
+	            )
+	            .logout((logout) -> logout
+	        		.logoutUrl("/member/logout")
+	        		.logoutSuccessUrl("/")
+	        		.clearAuthentication(true)
+	        		.invalidateHttpSession(true)
+	        		.permitAll());  						// 로그아웃도 모두 접근 가능
         return http.build();
     }
 
